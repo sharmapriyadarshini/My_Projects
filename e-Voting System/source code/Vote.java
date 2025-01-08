73aedff7531266c24f24af3aad5f846bdf315541
Priyadarshini Sharma <129146338+sharmapriyadarshini@users.noreply.github.com>
2025-01-08 17:20:54 -0600

    Add files via upload

---
+++ e-Voting System/source code/Vote.java
@@ -0,0 +1,285 @@
+/*
+	* Priyadarshini Sharma
+	* Pragati Thakur
+	* Rajani Devi
+	* Ritu
+	* Shardul Shandil
+	* Simple Applet Voting System
+*/
+
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+
+ public class Vote extends Applet
+{
+
+public int count1;
+public int count2;
+public int count3;
+public int count4;
+public int count5;
+public int count6;
+public int count7;
+public int count8;
+public Button vote;
+
+ TextField anc;
+ TextField np;
+ TextField ifp;
+ TextField azapo;
+ TextField udm;
+ TextField dp;
+ TextField cat;
+ TextField pac;
+
+
+public void init()
+
+ {
+ 
+ count1 =0;
+ count2 =0;
+ count3 =0;
+ count4 =0;
+ count5 =0;
+ count6 =0;
+ count7 =0;
+ count8 =0;
+ 
+
+     this.setBackground(Color.blue);
+
+     setLayout(new BorderLayout());
+     Panel p1 =new Panel();
+	 p1.setBackground(Color.lightGray);
+     p1.setLayout(new GridLayout(8,2,3,2));
+ 
+    // CheckboxGroup cg = new CheckboxGroup();
+	 Checkbox ch1 =new Checkbox("National Party");     
+         Checkbox ch2 =new Checkbox("African National Congress");     
+         Checkbox ch3 =new Checkbox("Democratic Party");
+	 Checkbox ch4 =new Checkbox("Inkatha Freedom Party");
+	 Checkbox ch5 =new Checkbox("United Democratic Movement");
+	 Checkbox ch6 =new Checkbox("Pan africanist Congress");
+	 Checkbox ch7 =new Checkbox("Azanian People Organization");
+	 Checkbox ch8=new Checkbox("Freedom Front");
+
+	anc = new TextField("Anc:"  + String.valueOf(count1));
+	anc.setEditable(false);
+
+        np = new TextField("Np:" + String.valueOf(count2));
+	np.setEditable(false);
+
+	dp = new TextField("Dp"  + String.valueOf(count3));
+	dp.setEditable(false);
+
+	ifp = new TextField("Ifp:" +  String.valueOf(count4));
+	ifp.setEditable(false);
+
+	pac = new TextField("Pac:" +  String.valueOf(count5));
+	pac.setEditable(false);
+
+        azapo = new TextField("Azapo:" + String.valueOf(count6));
+	azapo.setEditable(false);
+
+	cat = new TextField("FF:"  + String.valueOf(count7));
+	cat.setEditable(false);
+
+	udm = new TextField("Udm:"  + String.valueOf(count8));
+	udm.setEditable(false);
+
+	handlenp npp = new handlenp();
+	p1.add(ch1);
+	ch1.addItemListener(npp);
+    p1.add(np);
+
+	handleanc ancc = new handleanc();
+	p1.add(ch2);
+        ch2.addItemListener(ancc);
+    p1.add(anc);
+	
+        handledp dpp = new handledp();
+ 	p1.add(ch3);
+	ch3.addItemListener(dpp);
+    p1.add(dp);
+
+        handleifp ifpp = new handleifp();
+	p1.add(ch4);
+	ch4.addItemListener(ifpp);
+    p1.add(ifp);
+
+	handleudm udmm = new handleudm();
+	p1.add(ch5);
+	ch5.addItemListener(udmm);
+    p1.add(udm);
+	
+	handlepac pacc = new handlepac();
+ 	p1.add(ch6);
+	ch6.addItemListener(pacc);
+    p1.add(pac);
+	
+        handleazapo azapoo = new handleazapo();
+	p1.add(ch7);
+	ch7.addItemListener(azapoo);
+    p1.add(azapo);
+	
+	handlecat cats = new handlecat();
+ 	p1.add(ch8);
+	ch8.addItemListener(cats);
+    p1.add(cat);
+
+	add("North",p1);
+	
+	Label l =new Label("Click twice to choose the Party");
+	Font f = new Font("Helvetica",Font.BOLD,32);
+	l.setFont(f);
+
+	add("Center",l);
+
+        SingleAction sa = new SingleAction();
+		vote =new Button("Count number of votes");
+		vote.addActionListener(sa);
+		add("South",vote);
+
+	
+					     
+ }
+
+class  handleanc implements ItemListener
+{
+//int cont =0;
+
+public void itemStateChanged(ItemEvent ie)
+{
+
+if(ie.getStateChange()==ItemEvent.SELECTED)
+  count1++;
+  //else count1--;
+
+ 
+}
+}
+
+
+class  handlenp implements ItemListener
+	{
+
+
+	public void itemStateChanged(ItemEvent ie)
+	{
+
+	if(ie.getStateChange()==ItemEvent.SELECTED)
+  	++count2;
+	//else --count2;
+
+	}
+	}
+
+class  handlepac implements ItemListener
+	{
+	//int cont3 =count3;
+
+	public void itemStateChanged(ItemEvent ie)
+	{
+
+	if(ie.getStateChange()==ItemEvent.SELECTED)
+  	++count3;
+	//else --count3;
+
+ 
+	}
+	}
+
+class  handledp implements ItemListener
+	{
+	//int cont4 =count4;
+
+	public void itemStateChanged(ItemEvent ie)
+	{
+
+	if(ie.getStateChange()==ItemEvent.SELECTED)
+	++count4;
+	//else --count4;
+
+	}
+	}
+
+class  handleudm implements ItemListener
+	{
+	//int cont5 =count5;
+
+	public void itemStateChanged(ItemEvent ie)
+	{
+
+	if(ie.getStateChange()==ItemEvent.SELECTED)
+	++count5;
+	//else --count5;
+
+
+	}
+	}
+
+class  handlecat implements ItemListener
+	{
+	//int cont6 =count6;
+
+	public void itemStateChanged(ItemEvent ie)
+	{	
+
+	if(ie.getStateChange()==ItemEvent.SELECTED)
+	++count6;
+	//else --count6;
+
+	}
+	}
+
+class  handleifp implements ItemListener
+	{
+	//cont7 =count7;
+
+	public void itemStateChanged(ItemEvent ie)
+	{
+
+	if(ie.getStateChange()==ItemEvent.SELECTED)
+	++count7;
+	//else --count7;
+
+
+	}
+	}
+
+class  handleazapo implements ItemListener
+	{
+	//int cont8 =count8;
+
+	public void itemStateChanged(ItemEvent ie)
+	{
+
+	if(ie.getStateChange()==ItemEvent.SELECTED)
+  	++count8;
+  	//else --count8;
+
+	}
+	}
+
+
+class SingleAction implements ActionListener
+{
+	public void actionPerformed(ActionEvent e)
+	{
+	
+	
+		anc.setText("Anc :" + String.valueOf(count1));
+		np.setText("Np :" + String.valueOf(count2)); 
+		pac.setText("Pac :" + String.valueOf(count3)); 
+		dp.setText("Dp :" + String.valueOf(count4)); 
+		udm.setText("Udm :" + String.valueOf(count5)); 
+		cat.setText("FF :" + String.valueOf(count6)); 
+		ifp.setText("Ifp :" + String.valueOf(count7)); 
+		azapo.setText("Azapo :" + String.valueOf(count8));
+	}
+     }
+}
