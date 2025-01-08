a589e59b08c19fb502c2aca266b665869763aa46
Priyadarshini Sharma <129146338+sharmapriyadarshini@users.noreply.github.com>
2025-01-08 17:11:53 -0600

    Add files via upload

---
+++ Blood Bank Mnaagement Website/index1.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<title>culture of himachal pradesh</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="rstyle.css">
+		 <!--[if lt IE 9]>
+            <style>
+                header
+                {
+                    margin: 0 auto 20px auto;
+                }
+                #four_columns .img-item figure span.thumb-screen
+                {
+                    display:none;
+                }  
+            </style>
+        <![endif]-->
+	</head>
+	<body>
+		<div class="header">
+		<?php include('1head.html');?>
+		</div>
+	<div class="topnav">
+	</div>
+	<div class="slider">
+	<?php include('2sllider.html');?>
+		</div>
+	<div class="content">
+	<?php include('3content.html');?>
+		</div>
+	<div class="row">
+		<div class="col-9 col-m-9">
+		<?php include('4lcont.html');?>
+		</div>
+		<div class="col-3 col-m-12">
+			<div class="aside">
+			<?php include('5lcont.html');?>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+	<?php include('6foot.html');?>
+	 
+</div>
+</body>
+</html>
